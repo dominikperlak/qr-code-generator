5f3225bdf1e52c2cf8f029826253e79ad17dade1
dominikperlak <dominikperlak2@gmail.com>
2023-04-24 12:11:43 +0200

    Adding Antd

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import QRCode from 'qrcode'
 import { useState } from 'react'
+import { Button, Input } from 'antd'
+import 'antd/dist/antd'
+
 
 function App() {
-	const [url, setUrl] = useState('')
-	const [qr, setQr] = useState('')
+  const [url, setUrl] = useState('')
+  const [qr, setQr] = useState('')
 
-	const GenerateQRCode = () => {
-		QRCode.toDataURL(url, {
-			width: 800,
-			margin: 2,
-			color: {
-				dark: '#335383FF',
-				light: '#EEEEEEFF'
-			}
-		}, (err, url) => {
-			if (err) return console.error(err)
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, {
+      width: 800,
+      margin: 2,
+      color: {
+        dark: '#335383FF',
+        light: '#EEEEEEFF'
+      }
+    }, (err, url) => {
+      if (err) return console.error(err)
 
-			console.log(url)
-			setQr(url)
-		})
-	}
+      console.log(url)
+      setQr(url)
+    })
+  }
 
-	return (
-		<div className="app">
-			<h1>QR Generator</h1>
-			<input 
-				type="text"
-				placeholder="e.g. https://google.com"
-				value={url}
-				onChange={e => setUrl(e.target.value)} />
-			<button onClick={GenerateQRCode}>Generate</button>
-			{qr && <>
-				<img src={qr} />
-				<a href={qr} download="qrcode.png">Download</a>
-			</>}
-		</div>
-	)
+  return (
+    <div className="app">
+      <h1>QR Generator</h1>
+      <Input
+        placeholder="e.g. https://google.com"
+        value={url}
+        onChange={e => setUrl(e.target.value)}
+        style={{ maxWidth: '300px', marginRight: '1rem' }}
+      />
+      <Button type="primary" onClick={GenerateQRCode}>Generate</Button>
+      {qr && <>
+        <img src={qr} alt="QR Code" style={{ display: 'block', maxWidth: '480px', margin: '2rem auto' }} />
+        <Button type="link" href={qr} download="qrcode.png">Download</Button>
+      </>}
+    </div>
+  )
 }
 
-export default App;
+export default App
