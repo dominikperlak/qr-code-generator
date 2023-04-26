168fa26b6ad168d2f8a60e46a4fc9c39028c9e90
dominikperlak <dominikperlak2@gmail.com>
2023-04-26 14:21:29 +0200

    adding GH Pages and Toastify

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import QRCode from 'qrcode'
 import { useState } from 'react'
 import { Button, Input } from 'antd'
 import 'antd/dist/antd'
-
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const [url, setUrl] = useState('')
   const [qr, setQr] = useState('')
 
   const GenerateQRCode = () => {
+    if (url.trim() === '') {
+      toast.error('Missing text!')
+      return
+    }
+
     QRCode.toDataURL(url, {
       width: 800,
       margin: 2,
@@ -17,10 +23,15 @@ function App() {
         light: '#EEEEEEFF'
       }
     }, (err, url) => {
-      if (err) return console.error(err)
+      if (err) {
+        console.error(err)
+        toast.error('Missing task.')
+        return
+      }
 
       console.log(url)
       setQr(url)
+      toast.success('QR Code has been generated succesfully!')
     })
   }
 
@@ -28,7 +39,7 @@ function App() {
     <div className="app">
       <h1>QR Generator</h1>
       <Input
-        placeholder="e.g. https://google.com"
+        placeholder="np. https://google.com"
         value={url}
         onChange={e => setUrl(e.target.value)}
         style={{ maxWidth: '300px', marginRight: '1rem' }}
